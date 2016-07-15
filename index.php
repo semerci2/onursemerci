aed68bbbc24e76577f10367b27d0cb59e592a854
semerci2 <onursemerc@hotmail.com>
2016-07-15 12:57:00 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$kullaniciadi = $_POST['metin'];
+echo " <br/> Kullanıcı Adı:" . $kullaniciadi ;
+$sifre = $_POST ['sifre'];
+echo "<br/> Şifre: " . $sifre;
+$cinsiyet = $_POST['cinsiyet'];
+echo "<br/>Cinsiyet: " . $kullaniciadi;
+$mail = $_POST ['E-Mail'];
+echo "<br/> Mail Adresi: " . $mail;
+$mesaj = $_POST['mesaj'];
+echo "<br/> Mesaj: " . $mesaj;
+
+
+?>
